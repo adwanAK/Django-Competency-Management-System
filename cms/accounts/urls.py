7a24882bc6ea5184fe4639e4d66163335400ffc9
Adwan <adwan999@yahoo.com>
2018-11-13 01:10:17 +0300

    adding more to accounts

--- cms/accounts/urls.py
+++ cms/accounts/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 
 urlpatterns = [
     path('', views.update_profile, name='update_profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+
 
 
 ]
